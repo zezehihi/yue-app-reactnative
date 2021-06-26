286ba7ea0a14f22c53541b28aec5f8dba766892b
shike <2085739245@qq.com>
2021-06-26 22:05:18 +0800

    fix bug

--- src/pages/TabBar/index.js
+++ src/pages/TabBar/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, fromView} from 'react-native';
+import {View, fromView, Image} from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 import {
   color,
@@ -52,46 +52,67 @@ class TabBar extends Component {
   render() {
     const {tab1, tab2, selectedTab} = this.state;
     return (
-      <TabNavigator
-        tabBarUnderlineStyle={{borderWidth: 0}}
-        tabBarStyle={{
-          width: pxToDpW(700),
-          position: 'absolute',
-          left: '50%',
-          transform: [{translateX: -pxToDpW(700 / 2)}],
-          bottom: pxToDpH(110),
-          borderRadius: pxToDpH(70),
-          borderWidth: 0,
-          borderColor: '#00000000',
-          elevation: 10,
-          shadowColor: '#888888',
-        }}
-        tabBarShadowStyle={{backgroundColor: 'transparent'}}>
-        <TabNavigator.Item
-          selected={selectedTab === 'message'}
-          titleStyle={{fontSize: size.font3}}
-          selectedTitleStyle={{color: '#3075ab'}}
-          onPress={() => this.setState({selectedTab: 'message'})}
-          title="资讯">
-          <News />
-        </TabNavigator.Item>
-        <TabNavigator.Item
-          selected={selectedTab === 'index'}
-          titleStyle={{fontSize: size.font3}}
-          selectedTitleStyle={{color: '#3075ab'}}
-          onPress={() => this.setState({selectedTab: 'index'})}
-          title="戏剧">
-          <Index />
-        </TabNavigator.Item>
-        <TabNavigator.Item
-          selected={selectedTab === 'my'}
-          onPress={() => this.setState({selectedTab: 'my'})}
-          titleStyle={{fontSize: size.font3}}
-          selectedTitleStyle={{color: '#3075ab'}}
-          title="我的">
-          <My />
-        </TabNavigator.Item>
-      </TabNavigator>
+      <View style={{flex: 1}}>
+        <TabNavigator
+          tabBarUnderlineStyle={{borderWidth: 0}}
+          tabBarShadowStyle={{backgroundColor: 'transparent'}}
+          tabBarStyle={{
+            width: pxToDpW(700),
+            height: pxToDpW(100),
+            position: 'absolute',
+            left: '50%',
+            transform: [{translateX: -pxToDpW(700 / 2)}],
+            bottom: pxToDpH(110),
+            borderRadius: pxToDpH(70),
+            borderWidth: 0,
+            borderColor: '#00000000',
+            elevation: 10,
+            shadowColor: '#888888',
+            paddingBottom: pxToDpH(10),
+          }}
+          tabBarShadowStyle={{backgroundColor: 'transparent'}}
+          renderSelectedIcon={
+            <Image
+              source={{
+                uri: 'https://yun.1dtfc.com/yue/userAvatars/f47f2491-d39d-47f3-81e6-5855c558dc0e.jpg425a8b7e-1f95-46fa-b6a4-1791a90d985e.jpg',
+              }}
+              style={{width: '100%', height: '100%'}}
+            />
+          }
+          renderIcon={
+            <Image
+              source={{
+                uri: 'https://yun.1dtfc.com/yue/userAvatars/f47f2491-d39d-47f3-81e6-5855c558dc0e.jpg425a8b7e-1f95-46fa-b6a4-1791a90d985e.jpg',
+              }}
+              style={{width: '100%', height: '100%'}}
+            />
+          }>
+          <TabNavigator.Item
+            selected={selectedTab === 'message'}
+            titleStyle={{fontSize: size.font3}}
+            selectedTitleStyle={{color: '#3075ab'}}
+            onPress={() => this.setState({selectedTab: 'message'})}
+            title="资讯">
+            <News />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={selectedTab === 'index'}
+            titleStyle={{fontSize: size.font3}}
+            selectedTitleStyle={{color: '#3075ab'}}
+            onPress={() => this.setState({selectedTab: 'index'})}
+            title="戏剧">
+            <Index />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={selectedTab === 'my'}
+            onPress={() => this.setState({selectedTab: 'my'})}
+            titleStyle={{fontSize: size.font3}}
+            selectedTitleStyle={{color: '#3075ab'}}
+            title="我的">
+            <My />
+          </TabNavigator.Item>
+        </TabNavigator>
+      </View>
     );
   }
 }
