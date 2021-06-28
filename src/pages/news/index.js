1efbadcc510fa7e40d5626e60e170cf04c6321de
shike <2085739245@qq.com>
2021-06-28 14:12:04 +0800

    资讯页轮播图

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -3,7 +3,6 @@ import {
   Text,
   View,
   StatusBar,
-  Image,
   TouchableOpacity,
   StyleSheet,
   ImageBackground,
@@ -15,7 +14,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 import {color, pxToDpH, pxToDpW, layout, size} from '@/MyStyle';
 import {Carousel} from 'teaset';
 import SearchBar from '@/components/searchBar';
-import {Divider} from 'react-native-elements';
+import {Divider, Image} from 'react-native-elements';
 import Api from '@/api/api';
 import request from '@/services/request';
 import SoundPlayer from 'react-native-sound-player';
@@ -44,59 +43,84 @@ class Index extends Component {
     return (
       <ScrollView>
         <StatusBar backgroundColor={'transparent'} translucent={true} />
-        <View style={{paddingBottom: pxToDpH(100)}}></View>
-        {news.length != 0 ? (
-          news.map((v, i) => (
-            <TouchableOpacity
-              onPress={() => this.context.navigate('NewsDetail', v)}
-              key={i}
-              style={{
-                padding: size.globalPadding,
-                paddingBottom: pxToDpH(10),
-                paddingTop: pxToDpH(10),
-              }}>
-              <Text
+        {news.length != 0 && (
+          <View
+            style={{
+              marginTop: pxToDpH(100),
+              marginBottom: pxToDpH(80),
+              paddingLeft: size.globalPadding,
+              paddingRight: size.globalPadding,
+            }}>
+            <Carousel
+              interval={6000}
+              style={{height: pxToDpH(660)}}
+              control={true}>
+              {news.map((v, i) => (
+                <Image
+                  key={i}
+                  style={{height: pxToDpH(660)}}
+                  source={{uri: v?.newsimages[0].photolink}}
+                />
+              ))}
+            </Carousel>
+          </View>
+        )}
+        <View style={{paddingBottom: pxToDpH(100)}}>
+          {news.length != 0 ? (
+            news.map((v, i) => (
+              <TouchableOpacity
+                onPress={() => this.context.navigate('NewsDetail', v)}
+                key={i}
                 style={{
-                  color: '#256ec0',
-                  fontSize: pxToDpW(44),
-                  paddingBottom: pxToDpH(30),
+                  padding: size.globalPadding,
+                  paddingBottom: pxToDpH(10),
+                  paddingTop: pxToDpH(10),
                 }}>
-                {v.title}
-              </Text>
-              <View
-                style={{
-                  flexDirection: 'row',
-                }}>
-                {v.newsimages[0].photolink != null && (
-                  <Image
-                    source={{uri: v.newsimages[0].photolink}}
-                    style={{width: pxToDpW(300), height: pxToDpH(250)}}
-                  />
-                )}
+                <Text
+                  style={{
+                    color: '#256ec0',
+                    fontSize: pxToDpW(44),
+                    paddingBottom: pxToDpH(30),
+                  }}>
+                  {v.title}
+                </Text>
                 <View
                   style={{
-                    paddingLeft: pxToDpW(60),
-                    paddingRight: pxToDpW(60),
-                    width:
-                      v.newsimages[0].photolink != null ? pxToDpW(730) : '100%',
+                    flexDirection: 'row',
                   }}>
-                  <Text style={{color: '#6b7a85', fontSize: size.font2}}>
-                    {v.summary.length > 40
-                      ? v.summary.slice(0, 40) + '...'
-                      : v.summary}
-                  </Text>
+                  {v.newsimages[0].photolink != null && (
+                    <Image
+                      source={{uri: v.newsimages[0].photolink}}
+                      style={{width: pxToDpW(300), height: pxToDpH(250)}}
+                    />
+                  )}
+                  <View
+                    style={{
+                      paddingLeft: pxToDpW(60),
+                      paddingRight: pxToDpW(60),
+                      width:
+                        v.newsimages[0].photolink != null
+                          ? pxToDpW(730)
+                          : '100%',
+                    }}>
+                    <Text style={{color: '#6b7a85', fontSize: size.font2}}>
+                      {v.summary.length > 40
+                        ? v.summary.slice(0, 40) + '...'
+                        : v.summary}
+                    </Text>
+                  </View>
                 </View>
-              </View>
-              <Divider
-                orientation="vertical"
-                width={5}
-                style={{marginBottom: pxToDpH(20), marginTop: pxToDpH(20)}}
-              />
-            </TouchableOpacity>
-          ))
-        ) : (
-          <></>
-        )}
+                <Divider
+                  orientation="vertical"
+                  width={5}
+                  style={{marginBottom: pxToDpH(20), marginTop: pxToDpH(20)}}
+                />
+              </TouchableOpacity>
+            ))
+          ) : (
+            <></>
+          )}
+        </View>
       </ScrollView>
     );
   }
